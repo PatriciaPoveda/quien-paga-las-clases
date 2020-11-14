2670a999078a45c33b56ee8d6fb0ca833ef3c4ea
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-11-14 19:42:35 +0100

    fix buttons

--- src/components/Hija.js
+++ src/components/Hija.js
@@ -7,112 +7,118 @@ class Hija extends React.Component {
     this.llamarAMama = this.llamarAMama.bind(this);
     this.recordarMamallamarAbuela = this.recordarMamallamarAbuela.bind(this);
     this.state = {
-      styling: "hidden",
-      styling2: "hidden2",
+      isClosed: true,
+      isOpen: false,
     };
   }
   llamarAMama() {
     this.props.pagarClase();
 
-    this.setState({
-      styling: "show",
+    this.setState((prevState) => {
+      return { isClosed: !prevState.isClosed };
     });
   }
   recordarMamallamarAbuela() {
     this.props.RecordarLLamarAbuela();
-
-    this.setState({
-      styling2: "show",
+    this.setState((prevState) => {
+      return { isOpen: !prevState.isOpen };
     });
   }
   render() {
+    const hiddenClass = this.state.isClosed ? "hidden" : "";
+    const hiddenClass2 = this.state.isOpen ? "" : "hidden";
     return (
       <>
-        <p>
-          {this.props.story1}
-          <strong>
-            (función <em>pagarLaClase</em> en Madre.js)
-          </strong>
-        </p>
-        <p>
-          {this.props.story2}
-          <strong>
-            (pasa la función <em>pagarClase</em> en Madre.js)
-          </strong>
-        </p>
-        <p>
-          {this.props.story3}
-          <strong>
-            (recoge la función <em>pagarClase</em> en las props del constructor
-            en Hija.js)
-          </strong>
-          {this.props.story4}
-          {this.props.story5}
-        </p>
+        <section className="actOne">
+          <p>
+            {this.props.story1}
+            <strong>
+              (función <em>pagarLaClase</em> en Madre.js)
+            </strong>
+          </p>
+          <p>
+            {this.props.story2}
+            <strong>
+              (pasa la función <em>pagarClase</em> en Madre.js)
+            </strong>
+          </p>
+          <p>
+            {this.props.story3}
+            <strong>
+              (recoge la función <em>pagarClase</em> en las props del
+              constructor en Hija.js)
+            </strong>
+            {this.props.story4}
+            {this.props.story5}
+          </p>
 
-        <button onClick={this.llamarAMama}>Dar la clase</button>
-        <div className={`${this.state.styling}`}>
+          <button onClick={this.llamarAMama}>Dar la clase</button>
+          <div className={hiddenClass}>
+            <p>
+              <strong>
+                (evento: al hacer click en botón llama a la función{" "}
+                <em>llamarAMama</em> en Hija.js)
+              </strong>
+            </p>
+            <p>
+              {this.props.story6}{" "}
+              <strong>
+                (llama a la función <em>pagarClase</em> en Madre.js)
+              </strong>
+            </p>
+            <p>Y ahora mira la consola</p>
+          </div>
+        </section>
+        <section className="actTwo">
           <p>
+            {this.props.story7}
             <strong>
-              (evento: al hacer click en botón llama a la función{" "}
-              <em>llamarAMama</em> en Hija.js)
+              (función <em>pagaLaClaseAbuela</em> en Abuela.js)
+            </strong>
+            <strong>
+              (pasa la función <em>pagaClaseAbuela</em> en Abuela.js)
             </strong>
           </p>
           <p>
-            {this.props.story6}{" "}
+            {this.props.story8}
             <strong>
-              (llama a la función <em>pagarClase</em> en Madre.js)
-            </strong>
-          </p>
-          <p>Y ahora mira la consola</p>
-        </div>
-        <p>
-          {this.props.story7}
-          <strong>
-            (función <em>pagaLaClaseAbuela</em> en Abuela.js)
-          </strong>
-          <strong>
-            (pasa la función <em>pagaClaseAbuela</em> en Abuela.js)
-          </strong>
-        </p>
-        <p>
-          {this.props.story8}
-          <strong>
-            (recoge la función <em>pagaClaseAbuela</em> en las props del
-            constructor en Madre.js)
-          </strong>
-        </p>
-        <p>
-          {this.props.story9}{" "}
-          <strong>
-            (pasa la función <em>RecordarLLamarAbuela</em> en Madre.js)
-          </strong>
-        </p>
-        <p>
-          {this.props.story3}
-          <strong>
-            (recoge la función <em>RecordarLLamarAbuela</em> en las props del
-            constructor en Hija.js)
-          </strong>
-          {this.props.story10}
-          {this.props.story5}
-        </p>
-        <button onClick={this.recordarMamallamarAbuela}>Dar la clase</button>
-        <div className={`${this.state.styling2}`}>
-          <p>
-            <strong>
-              (evento: al hacer click en botón llama a la función{" "}
-              <em>recordarMamallamarAbuela</em> en Hija.js)
+              (recoge la función <em>pagaClaseAbuela</em> en las props del
+              constructor en Madre.js)
             </strong>
           </p>
           <p>
-            {this.props.story11}{" "}
+            {this.props.story9}{" "}
             <strong>
-              (llama a la función <em>recordarLLamarALaAbuela</em> en Madre.js)
+              (pasa la función <em>RecordarLLamarAbuela</em> en Madre.js)
             </strong>
           </p>
-          <p>Y ahora mira la consola</p>
-        </div>
+          <p>
+            {this.props.story3}
+            <strong>
+              (recoge la función <em>RecordarLLamarAbuela</em> en las props del
+              constructor en Hija.js)
+            </strong>
+            {this.props.story10}
+            {this.props.story5}
+          </p>
+          <button onClick={this.recordarMamallamarAbuela}>Dar la clase</button>
+          <div className={hiddenClass2}>
+            <p>
+              <strong>
+                (evento: al hacer click en botón llama a la función{" "}
+                <em>recordarMamallamarAbuela</em> en Hija.js)
+              </strong>
+            </p>
+            <p>
+              {this.props.story11}{" "}
+              <strong>
+                (llama a la función <em>recordarLLamarALaAbuela</em> en
+                Madre.js)
+              </strong>
+            </p>
+            <p>Y ahora mira la consola</p>
+          </div>
+        </section>
       </>
     );
   }
